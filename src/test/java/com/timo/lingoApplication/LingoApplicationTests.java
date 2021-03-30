038385cbc64df30a400a13b878856827af6b3784
TimovanDijk <43412697+TimovanDijk@users.noreply.github.com>
2021-03-30 13:51:58 +0200

    initial spring project

---
+++ src/test/java/com/timo/lingoApplication/LingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timo.lingoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
